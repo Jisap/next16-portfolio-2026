a826f0e8b8fb1b0fadf604810918596d9ef10e75
Jorge Matos <jisapjisap@gmail.com>
2026-06-24 18:55:17 +0200

    feat: implement TextHoverEffect component, update motion library, and add footer with real-time clock

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+"use client"
+
+import { useEffect, useState } from "react"
+import { TextHoverEffect } from "./ui/text-hover-effect";
+
+
+const formatTime = () => new Date().toLocaleTimeString(); // Muestra la hora actual
 
 const Footer = () => {
-    return (
-        <div>Footer</div>
-    )
+
+  const [timeNow, ssetTimeNow] = useState<string>("");
+
+  useEffect(() => {
+    const initialId = window.setTimeout(() => setTimeout(formatTime()), 0);       // se asigna un id con 0 milisegundos de delay
+    const intervalId = window.setInterval(() => ssetTimeNow(formatTime()), 1000); // se asigna un id con 1000 milisegundos de delay
+    return () => {
+      window.clearTimeout(initialId)
+      window.clearInterval(intervalId)
+    }
+  }, [])
+
+  return (
+    <footer className="relative mx-auto pb-12 md:pb-0 lg:pb-0 xl:pb-0 max-w-303 bg-zinc-950 border-t border-zinc-800 text-zinc-300 overflow-hidden">
+      <div className="flex flex-col sm:flex-row items-center justify-between gap-4 px-20 py-5">
+        <div className="text-lg cursor-pointer">Reach out →</div>
+        <TextHoverEffect text="ACET" />
+        <div className="text-lg">{timeNow}</div>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
