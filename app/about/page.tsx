2702f3cd2360647f6ab25cc86344d0be1289dc23
Jorge Matos <jisapjisap@gmail.com>
2026-06-29 16:20:17 +0200

    feat: add BackgroundBeamsWithCollision component and initialize about page layout

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,52 @@
-import React from 'react'
+"use client"
 
-const page = () => {
-    return (
-        <div>page</div>
-    )
+
+import Link from "next/link"
+import { aboutPageStyles as a } from "../data/dummyStyles"
+
+
+
+
+
+const About = () => {
+
+  const interests = [
+    "FULL-STACK DEV",
+    "AI ENGINEER",
+    "LLMS",
+    "TRAVEL",
+    "MUSIC",
+    "F1",
+    "READING",
+  ];
+
+  const techStack = [
+    "React",
+    "LLM",
+    "Rails",
+    "Next.js",
+    "TypeScript",
+    "Postgres",
+    "Redis",
+  ];
+
+  const email = "jisapdev@gmail.com"
+
+  const gmailComposeUrl = `https://mail.google.com/mail/?view=cm&fs=1&to=${encodeURIComponent(email)}`;
+
+  return (
+    <div className={a.pageContainer}>
+      <div className={a.contentContainer}>
+        <div className={a.backgroundContainer}>
+          <div className={a.backgroundContainer}>
+
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+  )
 }
 
-export default page
+export default About
